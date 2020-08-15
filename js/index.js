0c1bdba789086a287f739c3cf3a4b817fd9dab8d
pore0007 <65929893+pore0007@users.noreply.github.com>
2020-08-14 22:33:52 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
+/*** references to  HTML elements ***/
+const container = document.querySelectorAll('.main-content'); // event target object
+const links = document.querySelector('.nav a');
 
+let url = '/partials/home.html';
+
+function loadContent(url)
+{
+  fetch(url)
+  .then(function (rsp)
+        {
+    return rsp.text();
+        })
+    
+  .then(function (data) 
+  {
+    container.innerHTML = data;
+  });
+}
+
+loadContent(url);
+function selectContent(event)
+{
+    event.preventDefault();
+    let clicked = event.target.href;
+    loadContent(clicked);
+}
+
+for (let i = 0; i < links.length; i++)
+{
+    links[i].addEventListener("click", selectContent);
+}
